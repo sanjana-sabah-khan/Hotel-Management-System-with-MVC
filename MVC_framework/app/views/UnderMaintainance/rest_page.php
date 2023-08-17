0e180706da2aef5d3e0df8fb9220ac19a39052c1
sanjana-sabah-khan <79992325+sanjana-sabah-khan@users.noreply.github.com>
2023-08-17 14:25:34 +0600

    Create rest_page.php

---
+++ MVC_framework/app/views/UnderMaintainance/rest_page.php
@@ -0,0 +1,252 @@
+
+
+<!-- RESTAURANT MENU PAGE DUMMY. RESERVES RESTAURANT -->
+
+
+
+<!doctype html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href=" <?php echo URLROOT ?>/public/css/stylesheet1.css">
+        <link rel="stylesheet" href=" <?php echo URLROOT ?>/public/css/restaurant.css">
+      
+<!-- ADDITIONAL STYLESHEET HERE -->
+        
+        <title>Menu page - RTH</title>
+    </head>
+    
+    
+    
+    <body>
+        
+<!-- ALL OF YOUR SITE CODE HERE -->
+        
+        <div class="container-fluid">
+            
+           
+<!-- NAVIGATION BAR -->
+            
+            
+            <?php
+                
+                require APPROOT . '/views/navigationBar.php';
+    
+            ?>
+            
+            
+            
+<!-- MENU SECTION -->
+            
+            
+            <h2 style="text-align: center; margin-top: 60px; margin-bottom: 40px;">MENU</h2>
+            
+            <div class="row">
+                <div class="offset-md-1 col-md-10">
+                    
+                    <div class="menu_content_box">   
+            
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content">
+                                    
+                                    <h3>Pizza</h3>
+                                    <br>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectetur. Sed at diam eget libero efficitur rutrum nec nec magna.
+                                    </p>
+                                    
+                                    <p style="font-size: 14pt">                                   
+                                        <b><u>Price:</u> BDT 500</b>
+                                    </p>
+                                          
+                                </div>
+                            </div>
+                            
+                                                       
+                            <div class="col-md-4">
+                                <div class="menu_item_1"> </div>
+                            </div>
+                        
+                        </div>
+                        
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content">
+                                    
+                                    <h3>Burger</h3>
+                                    <br>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectetur. Sed at diam eget libero efficitur rutrum nec nec magna.
+                                    </p>
+                                    
+                                    <p>
+                                        <b style="font-size: 14pt"><u>Price:</u> BDT 500</b>                                    
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="col-md-4">
+                                <div class="menu_item_2"> </div>
+                            </div>
+                        
+                        </div>
+                        
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content"> 
+                                    
+                                    <h3>Pasta</h3>
+                                    <br>
+                                    <p>                      
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectetur. Sed at diam eget libero efficitur rutrum nec nec magna.
+                                    </p>
+                                    
+                                     <p>                                   
+                                        <b style="font-size: 14pt"><u>Price:</u> BDT 500</b>                                  
+                                    </p>
+                                    
+                                </div>                
+                            </div>
+                      
+                      
+                            <div class="col-md-4">                 
+                                <div class="menu_item_3"> </div>
+                            </div>
+                        
+                        </div>
+                                      
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content">
+                                    
+                                    <h3>Cake</h3>
+                                    <br>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectetur. Sed at diam eget libero efficitur rutrum nec nec magna.
+                                    </p>
+                                    
+                                    <p>                                   
+                                        <b style="font-size: 14pt"><u>Price:</u> BDT 500</b>                                    
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="col-md-4">
+                                <div class="menu_item_4"> </div>
+                            </div>
+                        
+                        </div>
+                        
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content">
+                                    
+                                    <h3>Coffee</h3>
+                                    <br>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectetur.
+                                    </p>
+                                    
+                                    <p>                                   
+                                        <b style="font-size: 14pt"><u>Price:</u> BDT 500</b>                                   
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="col-md-4">
+                                <div class="menu_item_5"> </div>
+                            </div>
+                        
+                        </div>
+                        
+                        
+                        <div class="row">
+                            
+                            <div class="offset-md-1 col-md-6">
+                                <div class="text_content">
+                                    
+                                    <h3>Carbonated Beverage</h3>
+                                    <br>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in mattis magna. Aliquam fermentum porta est, ac ullamcorper justo porttitor id. Donec tincidunt sed velit ut consectet.
+                                    </p>
+                                    
+                                    <p>                                    
+                                        <b style="font-size: 14pt"><u>Price:</u> BDT 500</b>                                   
+                                    </p>
+                                    
+                                </div>
+                            </div>
+                            
+                            
+                            <div class="col-md-4">
+                                <div class="menu_item_6"> </div>
+                            </div>
+                        
+                        </div>
+                        
+                        
+   <!-- BUTTON TO RESERVE -->
+                        
+                        <div class="row">
+                            <div class="offset-md-1 col-md-6">
+                                
+                                <a href=" <?php echo URLROOT ?>/Reservations/restaurants" class="btn btn-info btn-block" role="button">Place Order</a>
+                                
+                            </div>
+                        </div>
+                        <br><br><br>
+                        
+                        
+                    </div>
+                
+                </div>
+            </div>
+            
+                     
+<!--FOOTER-->
+            
+            
+            <?php
+                
+                require APPROOT . '/views/footer.php';
+    
+            ?>
+            
+            
+
+        </div>       
+        
+<!-- ALL OF YOUR SITE CODE HERE -->
+        
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+<!-- ADDITIONAL JS HERE -->
+    
+    </body>
+    
+</html>
