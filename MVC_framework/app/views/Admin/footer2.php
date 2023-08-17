6dcebae8bf794f5ae672a1d0dc305d31e5c46675
sanjana-sabah-khan <79992325+sanjana-sabah-khan@users.noreply.github.com>
2023-08-17 14:22:57 +0600

    Create footer2.php

---
+++ MVC_framework/app/views/Admin/footer2.php
@@ -0,0 +1,66 @@
+<!-- FOOTER FOR ADMIN -->
+            
+            <div class="row">
+                <div class="col-md-12 p-0">
+                    
+                    <footer class="mainfooter" role="contentinfo">
+                        <div class="footer-middle">
+                            
+                            <div class="container">
+                                
+                                <div class="row">
+                                
+                                
+   <!-- Column1: SITE MAP -->                                  
+                             
+                                    <div class="col-md-3 col-sm-6">    
+                                        <div class="footer-pad" style= "padding-left: 70px;">
+                                            <h4>Site Map</h4>
+
+                                            <ul class="list-unstyled">
+                                                <li><a href="admin_home.php">Admin HomePage</a></li>
+                                                <li><a href="handle_room.php">Check Room Status</a></li>
+                                                <li><a href="userdetails.php">View User Details</a></li>
+                                                <li><a href="usercomment.php">View User Comments</a></li>
+                                            </ul>
+
+                                        </div>
+                                    </div>
+                                
+                                
+    <!--Column3: FOLLOW US-->
+
+                                    <div class="offset-md-3 col-md-3" style= "padding-left: 150px;">
+                                        <h4>Follow Us</h4>
+
+                                        <ul class="social-network social-circle">
+                                            <li><a href="https://www.facebook.com/" class="icoFacebook" title="Facebook"><i class="fab fa-facebook-f"></i></a></li>
+                                            <li><a href="https://www.linkedin.com/" class="icoLinkedin" title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
+                                        </ul>
+
+                                    </div>
+                                    
+                                </div>
+                                                                             
+   <!-- COPYRIGHT -->                                
+                                
+                                <div class="row">
+                                    <div class="col-md-12 copy">
+                                        
+                                        <p class="text-center">
+                                            &copy; Copyright 2021 - Royal Tulip Hotels.  All rights reserved by Sanjana Sabah Khan, CSE391 Spring 2021.
+                                        </p>
+                                        
+                                    </div>
+                                </div>
+                            
+                            
+                            </div>
+                            
+                        </div>
+                    </footer>
+                
+                </div>
+            </div>
+        
+        
