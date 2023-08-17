8748a00a4f44c15f2478e9caea0936f36a555493
sanjana-sabah-khan <79992325+sanjana-sabah-khan@users.noreply.github.com>
2023-08-17 14:20:24 +0600

    Create about.php

---
+++ MVC_framework/app/views/about.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href=" <?php echo URLROOT ?> /public/css/stylesheet1.css">
+        
+<!-- ADDITIONAL STYLESHEET HERE -->
+          
+        <title>About - RTH</title>
+    </head>
+    
+    
+    
+    <body>
+            
+<!-- ALL OF YOUR SITE CODE HERE --> 
+        
+        <div class="container-fluid">
+       
+            
+<!-- NAVIGATION BAR -->
+            
+            
+            <?php
+                
+                require APPROOT . '/views/navigationBar.php';
+    
+            ?>
+            
+            
+            
+<!-- ABOUT IMAGE -->
+            
+            <div class="row">
+                <div class="col-md-12 col-lg-12 col-sm-12 col-12 p-0">
+                    
+                    <div class="hero_section_about"> </div>
+                    
+                </div>
+            </div>
+            
+            
+<!-- LOGO -->
+            
+            <div class="row">
+                <div class="col-md-12 p-0">
+                    
+                    <div class="card mx-auto" style="width: 18rem;">
+                        <img class="card-img-top" src=" <?php echo URLROOT ?> /public/images/Royal_Tulip_Hotel_Logo.png" alt="Card image cap">
+                    </div>
+                
+                </div>
+            </div>
+            
+                 
+<!-- MAIN CONTENT -->            
+            
+            <div class="row">
+                <div class="offset-md-2 col-md-8">
+                     
+                    <div class="content_box_about">
+                        
+                        
+                        <h3 style="margin-top: 20px; margin-bottom: 30px;"> About Our Hotels </h3>
+                        
+                        <p style="margin-bottom: 50px;">
+                            Pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sitet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit.
+                        </p>
+                        
+                        
+                        <div class="about_image_1"> </div>                           
+                           
+                            
+                        <p style="margin-top: 50px; margin-bottom: 50px;">
+                            pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit. Pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum vari.                   
+                        </p>
+                        
+                        
+                        <div class="about_image_2"> </div>
+                    
+          
+                        <h3 style="margin-top: 80px; margin-bottom: 30px;"> Brief Look Into Our History </h3>
+                        
+                        <p style="margin-bottom: 50px;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Feugiat in fermentum posuere urna. Porta nibh venenatis cras sed felis. Et molestie ac feugiat sed lectus vestibulum mattis. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Etiam erat velit scelerisque in dictum. Placerat duis ultricies lacus sed turpis tincidunt id aliquet risus. Pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sitit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo donec enim diam vulputate ut pharetra.
+                        </p>
+                        
+                        
+                        <div class="about_image_3"> </div>
+                        
+                        
+                        <p style="margin-top: 50px; margin-bottom: 80px;">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et magnis dis parturient montes nascetur ridiculus mus. Laoreet sit amet cursus sit amet dictum sit. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id.
+                        </p>
+                        
+                        
+                    </div>
+                    
+                </div>
+            </div>
+            
+                  
+<!--FOOTER-->
+            
+            
+            <?php
+                
+                require APPROOT . '/views/footer.php';
+    
+            ?>
+            
+            
+            
+        </div>       
+        
+        
+<!-- ALL OF YOUR SITE CODE HERE -->
+        
+        
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        
+<!-- ADDITIONAL JS HERE -->
+    
+    </body>
+    
+</html>
+          
+          
