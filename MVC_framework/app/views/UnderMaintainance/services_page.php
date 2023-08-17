8f99fed6a18cfff8cbc27ea87ee64956e49a562a
sanjana-sabah-khan <79992325+sanjana-sabah-khan@users.noreply.github.com>
2023-08-17 14:25:57 +0600

    Add files via upload

---
+++ MVC_framework/app/views/UnderMaintainance/services_page.php
@@ -0,0 +1,66 @@
+
+
+<!-- SERVICE DUMMY: UNDERCONSTRUCTION -->
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href=" <?php echo URLROOT ?> /public/css/undermaintain.css">
+
+<!-- ADDITIONAL STYLESHEET HERE -->
+
+    <title>Services Dummy</title>
+</head>
+
+
+
+<body>
+
+<!-- ALL OF YOUR SITE CODE HERE -->
+
+    <div class="container-fluid">
+
+
+<!-- Under Construction Page -->
+
+        <div class="row">
+            <div class="offset-md-2 col-md-8 p-0">
+
+                <div class="error-image">
+                    <h1>👨‍🔧</h1>
+                </div>
+                
+                
+                <div class="error-msg-container">
+                    <h1>This Page is Under Construction</h1>
+                    <p>We expect this page to be ready soon.</p>
+                    <p>We apologize for any inconvenience.</p>
+                    <a href=" <?php echo URLROOT ?>/Homes/services"><p>Go back to Services</p></a>
+                </div>
+
+            </div>
+        </div>
+
+        
+    </div>
+
+
+
+<!-- ALL OF YOUR SITE CODE HERE -->
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+<!-- ADDITIONAL JS HERE -->
+
+</body>
+
+</html>
